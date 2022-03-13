0d18193b973be119f690e0a3ad2634fb115588f0
SerjMartin <serjmartin@mail.com>
2022-03-13 22:24:39 +0000

    Update treatments.html

--- treatments/models.py
+++ treatments/models.py
@@ -11,8 +11,9 @@ class Category(models.Model):
 class Treatment(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    deals = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
