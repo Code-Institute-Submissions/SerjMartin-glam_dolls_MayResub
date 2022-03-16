5ede5acbb16acbba3cb285a694d84a6739590f26
SerjMartin <serjmartin@mail.com>
2022-03-16 14:35:07 +0000

    Add Content html to Eye_care, Nail_care and facial pages

--- treatments/urls.py
+++ treatments/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_treatments, name='treatments'),
-    path('eye_care/', views.eye_care, name='eye_care')
+    path('eye_care/', views.eye_care, name='eye_care'),
+    path('nail_care/', views.nail_care, name='nail_care'),
+    path('facial/', views.facial, name='facial'),
 ]
--- treatments/views.py
+++ treatments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Treatment
 
+
 def all_treatments(request):
     """A view to show all treatments"""
 
@@ -12,6 +13,7 @@ def all_treatments(request):
 
     return render(request, 'treatments/treatments.html', context)
 
+
 def eye_care(request):
     """A view to return the eye_care view"""
 
@@ -21,4 +23,28 @@ def eye_care(request):
         'treatments': treatments,
     }
 
-    return render(request, 'treatments/eye_care.html')
+    return render(request, 'treatments/eye_care.html', context)
+
+
+def nail_care(request):
+    """A view to return the nail_care view"""
+
+    treatments = Treatment.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+
+    return render(request, 'treatments/nail_care.html', context)
+
+
+def facial(request):
+    """A view to return the facial view"""
+
+    treatments = Treatment.objects.all()
+
+    context = {
+        'treatments': treatments,
+    }
+
+    return render(request, 'treatments/facial.html', context)
