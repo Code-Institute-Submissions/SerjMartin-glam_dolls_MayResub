ae38442b4bbb89f39ce48b8efd514dafa04f2070
SerjMartin <serjmartin@mail.com>
2022-04-19 15:42:17 +0000

    Fix issues as line length in Python code

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
     """View the bag contents"""
 
--- treatments/models.py
+++ treatments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=254)
     image = models.ImageField(null=True, blank=True)
@@ -9,7 +10,8 @@ class Category(models.Model):
 
 
 class Treatment(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
