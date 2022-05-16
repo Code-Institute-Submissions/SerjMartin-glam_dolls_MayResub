2b979cf227ee3211df4ec619b250957df7b5c827
SerjMartin <serjmartin@mail.com>
2022-05-16 17:15:50 +0000

    Product Admin - add product view, urls and templates

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -12,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
