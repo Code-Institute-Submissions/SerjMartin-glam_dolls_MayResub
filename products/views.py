5bc18d3a83a9811857fd6f82e4c94f0e7f9e529c
SerjMartin <serjmartin@mail.com>
2022-05-16 18:29:36 +0000

    Product Admin - Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 from .models import Product
 from .forms import ProductForm
 
@@ -17,8 +18,18 @@ def all_products(request):
 
 def add_product(request):
     """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('products'))
+        else:
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
 
-    form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
